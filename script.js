538290df6ad6f6d5d8d2323f18e0c4709e3f6a3a
hitesh911khatri <149157855+hitesh911khatri@users.noreply.github.com>
2024-02-10 02:57:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let container = document.createElement("div")
+container.classList.add("conatiner")
+
+let span = document.getElementById("span")
+span.innerHTML = 10
+
+document.body.append(container)
+let count = 10;
+setTimeout(() => {
+    count = count - 1
+    span.innerHTML = count
+    setTimeout(() => {
+        count = count - 1
+        span.innerHTML = count
+        setTimeout(() => {
+            count = count - 1
+            span.innerHTML = count
+            setTimeout(() => {
+                count = count - 1
+                span.innerHTML = count
+                setTimeout(() => {
+                    count = count - 1
+                    span.innerHTML = count
+                    setTimeout(() => {
+                        count = count - 1
+                        span.innerHTML = count
+                        setTimeout(() => {
+                            count = count - 1
+                            span.innerHTML = count
+                            setTimeout(() => {
+                                count = count - 1
+                                span.innerHTML = count
+                                setTimeout(() => {
+                                    count = count - 1
+                                    span.innerHTML = count
+                                    setTimeout(() => {
+                                        count = count - 1
+                                        span.innerHTML = "happy independence day"
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
